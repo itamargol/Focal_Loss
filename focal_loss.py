d8a714c9f58d1f168d088cb9f5fef3ee47f9d6f4
itamargol <50107945+itamargol@users.noreply.github.com>
2020-05-08 13:10:04 +0300

    Update focal_loss.py

--- focal_loss.py
+++ focal_loss.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 
 # Compatible with tensorflow backend
 
-def focal_loss(gamma=2., alpha=.25):
-    def focal_loss_fixed(y_true, y_pred):
-        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
-        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-        return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1+K.epsilon())) - K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0+K.epsilon()))
-    return focal_loss_fixed
+def focal_loss(alpha=0.25,gamma=2.0):
+    def focal_loss_function(Y_real, Y_hat):
+        pt0 = tf.where(tf.equal(Y_real, 0), Y_hat, tf.zeros_like(Y_hat))
+        pt1 = tf.where(tf.equal(Y_real, 1), Y_hat, tf.ones_like(Y_hat))
+        return -K.mean(alpha * K.pow(1.0 - pt1, gamma) * K.log(pt1+K.epsilon())) - K.mean((1 - alpha) * K.pow(pt0, gamma) * K.log(1.0 - pt0+K.epsilon()))
+    return focal_loss_function
